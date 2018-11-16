9fb6bec9a4c24af1cbc08bc7c9b49cf597e73bf3
Kostas Filokostas <filokostas@gmail.com>
2018-11-16 23:21:51 +0200

    prepare for CI/CD

--- src/app/pos/pos.module.ts
+++ src/app/pos/pos.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
 
 import { PosRoutingModule } from './pos-routing.module'
 import { PosComponent } from './pos/pos.component'
